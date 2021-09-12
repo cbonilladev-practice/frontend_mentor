38f1fbf0e4a18b4b805bce5aace70d509b9752d0
cabonilla <christian.bonilla01@gmail.com>
2021-09-12 10:10:34 -0700

    Task, completed. CSS could be refined, but most responsive & all input functionality done.

--- Intro component with sign-up form/index.js
+++ Intro component with sign-up form/index.js
@@ -1,1 +1,48 @@
-console.log("Hello, Laverne.")
+const form = document.getElementById('form_input');
+
+function addError(param) {
+	param.parentNode.classList.add('error');
+}
+
+function removeError(param) {
+	param.parentNode.classList.remove('error');
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const firstName = form['first_name'];
+	const lastName = form['last_name'];
+	const emailAddress = form['email_address'];
+	const password = form['password'];
+
+	if (firstName.value === "") {
+		addError(firstName)
+	} else {
+		removeError(firstName)
+	}
+
+	if (lastName.value === "") {
+		addError(lastName)
+	} else {
+		removeError(lastName)
+	}
+
+	if (!validateEmail(emailAddress.value)) {
+		addError(emailAddress)
+	} else {
+		removeError(emailAddress)
+	}
+
+	if (password.value === "") {
+		addError(password)
+	} else {
+		removeError(password)
+	}
+
+})
