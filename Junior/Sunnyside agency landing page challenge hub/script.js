24794e8fc4d6bc2da437d0bba0dbbcadebfb73be
cabonilla <christian.bonilla01@gmail.com>
2021-10-03 13:55:44 -0700

    Added finishing touches, mobile-responsiveness and interactivity. Not bad.

--- Junior/Sunnyside agency landing page challenge hub/script.js
+++ Junior/Sunnyside agency landing page challenge hub/script.js
@@ -1,5 +1,10 @@
-console.log("Hello, Laverne.")
+const hamburger = document.getElementById("hamburger");
+const hamburger_menu = document.getElementById("nav_links");
 
-if (window.innerWidth <= 1440) {
-	console.log("This is 1440px.")
+function revealHamburger() {
+	if (hamburger_menu.classList[1] === "hide") {
+		hamburger_menu.setAttribute("class", "nav_links show")
+	} else {
+		hamburger_menu.setAttribute("class", "nav_links hide")
+	}
 }
